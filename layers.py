253147ff5f6e0d516189af7a0506c03ce6da7e4a
KovalenkoAE <98532402+KovalenkoAE@users.noreply.github.com>
2022-06-29 00:19:02 +0300

    Update layers.py

--- layers.py
+++ layers.py
@@ -1,3 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
 # Graph Convolutional Layer:
 class GCNLayer(nn.Module):
     def __init__(self, in_dim, out_dim):
@@ -15,19 +20,31 @@ class GCNLayer(nn.Module):
     
 # A = ReLu(W)    
 class Graph_ReLu_W(nn.Module):
-    def __init__(self, nnodes, device):
+    def __init__(self, num_nodes, k, device):
         super(Graph_Relu_W, self).__init__()
-        self.nnodes = nnodes
-        self.A = nn.Parameter(torch.randn(nnodes, nnodes).to(device), requires_grad=True).to(device)
+        self.num_nodes = num_nodes
+        self.k = k
+        
+        self.A = nn.Parameter(torch.randn(num_nodes, num_nodes).to(device), requires_grad=True).to(device)
 
     def forward(self, idx):
-        return F.relu(self.A)
+        
+        adj = F.relu(self.A)
+        
+        if self.k:
+            mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
+            mask.fill_(float('0'))
+            s1,t1 = (adj + torch.rand_like(adj)*0.01).topk(self.k,1)
+            mask.scatter_(1,t1,s1.fill_(1))
+            adj = adj*mask
+            
+        return adj
 
     
 # A for Directed graphs:
 class Graph_Directed_A(nn.Module):
       
-    def __init__(self, num_nodes, window_size, alpha, k=None, device):
+    def __init__(self, num_nodes, window_size, alpha, k, device):
         super(Graph_Directed_A, self).__init__()
         
         self.alpha = alpha
@@ -58,7 +75,7 @@ class Graph_Directed_A(nn.Module):
 # A for Uni-directed graphs:
 class Graph_Uni_Directed_A(nn.Module):
       
-    def __init__(self, num_nodes, window_size, alpha, k=None, device):
+    def __init__(self, num_nodes, window_size, alpha, k, device):
         super(Graph_Directed_A, self).__init__()
         
         self.alpha = alpha
@@ -74,7 +91,7 @@ class Graph_Uni_Directed_A(nn.Module):
         
         m1 = torch.tanh(self.alpha*self.l1(self.e1(idx)))
         m2 = torch.tanh(self.alpha*self.l2(self.e2(idx)))
-        adj = F.relu(torch.tanh(self.alpha*(torch.mm(m1, m2.transpose(1,0)) - torch.mm(m2, m1.transpose(1,0)))
+        adj = F.relu(torch.tanh(self.alpha*(torch.mm(m1, m2.transpose(1,0)) - torch.mm(m2, m1.transpose(1,0)))))
         
         if self.k:
             mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
@@ -89,7 +106,7 @@ class Graph_Uni_Directed_A(nn.Module):
 # A for Undirected graphs:
 class Graph_Undirected_A(nn.Module):
       
-    def __init__(self, num_nodes, window_size, alpha, k=None, device):
+    def __init__(self, num_nodes, window_size, alpha, k, device):
         super(Graph_Directed_A, self).__init__()
         
         self.alpha = alpha
